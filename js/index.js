b1f3561c570f72b3d1fb37705c40a3aca6c23d8a
hweathers <hweathers@vcd-ch204-04.eastern.ewu.edu>
2018-01-31 13:27:24 -0800

    v0

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/*
+* UIkit : https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js
+* UIkit Icons: https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js
+*/
